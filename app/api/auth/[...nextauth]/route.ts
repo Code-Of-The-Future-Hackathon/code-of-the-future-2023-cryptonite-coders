d12c92835809db8748f12e4f87c83df44bcc2cac
Ivo <ivo.kraev2@protonmail.com>
2023-12-09 11:25:35 +0200

    refactor: Change file for the auth options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,7 @@
-import NextAuth, {type User} from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import NextAuth from "next-auth"
+import {authOptions} from "@/lib/auth";
 
 
-const handler = NextAuth({
-    providers: [
-        CredentialsProvider({
-            name: "login",
-            credentials: {
-                username: {label: "username", type: "text"},
-                password: {label: "password", type: "password"},
-            },
-            authorize: async (credentials, req) => {
-
-                const obj: User = {
-                    id: "1",
-                    name: "test",
-                    email: "test@example.com"
-                }
-
-                return obj;
-            }
-        })
-    ],
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST}
