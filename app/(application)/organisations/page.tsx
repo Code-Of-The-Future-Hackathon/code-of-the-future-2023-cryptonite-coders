3f926e64e271e52296dac037347f2fa826d1b3f7
Hristiyan <hristiyandimitrov26@itpg-varna.bg>
2023-12-10 00:07:17 +0200

    fix: Fix organisations styling

--- app/(application)/organisations/page.tsx
+++ app/(application)/organisations/page.tsx
@@ -19,18 +19,20 @@ export default async function OrganisationsPage() {
     });
 
     return organisations.length > 0 ? (
-        organisations.map((organisation, index) => (
-            <div className="flex min-h-screen flex-col items-center justify-between p-2 gap-3 sm:p-10">
-                <OrganisationParent
-                    id={organisation.id}
-                    name={organisation.name}
-                    image={
-                        organisation.image ||
-                        "https://api-private.atlassian.com/users/be0f5f0009c0c0551a411dd9bc1fdc85/avatar"
-                    }
-                />
+        <div className="min-h-[calc(100vh-11rem)]">
+            <div className="grid grid-cols-1 grid-rows-auto grid-flow-row gap-x-20 gap-y-5 lg:grid-cols-3 sm:grid-cols-2">
+                {organisations.map((organisation, index) => (
+                    <OrganisationParent
+                        id={organisation.id}
+                        name={organisation.name}
+                        image={
+                            organisation.image ||
+                            "https://api-private.atlassian.com/users/be0f5f0009c0c0551a411dd9bc1fdc85/avatar"
+                        }
+                    />
+                ))}
             </div>
-        ))
+        </div>
     ) : (
         <Typography variant="h1" className="h-[calc(100vh-15rem)]">
             There is no organizations!
