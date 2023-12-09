2762b47ff2f2950b8375523997289379ca233783
Stoil100 <toli100ilov@gmail.com>
2023-12-09 20:57:17 +0200

    fix: Fix register link leading to the same page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
                 <div className=" w-full flex justify-center items-center flex-col gap-4 bg-zinc-900/60 p-7">
                     <p className="text-5xl ">Welcome back!</p>
                     <p className="text-gray-400">Login to continue</p>
-                    <Link href={"register"}><p  className="bg-amber-400 rounded-full h-[40px] w-[300px] flex justify-center items-center text-2xl hover:scale-110 transition-all">Log in</p></Link>
+                    <Link href={"login"}><p  className="bg-amber-400 rounded-full h-[40px] w-[300px] flex justify-center items-center text-2xl hover:scale-110 transition-all">Log in</p></Link>
                 </div>
             </div>
         </main>
