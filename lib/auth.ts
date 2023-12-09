2aea59ff60622278d287fb63585a114493aec7ea
Ivo <ivo.kraev2@protonmail.com>
2023-12-09 11:25:35 +0200

    refactor: Change file for the auth options

---
+++ lib/auth.ts
@@ -0,0 +1,27 @@
+import {NextAuthOptions, type User} from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "login",
+            credentials: {
+                username: {label: "username", type: "text"},
+                password: {label: "password", type: "password"},
+            },
+            authorize: async (credentials, req) => {
+
+                const obj: User = {
+                    id: "1",
+                    name: "test",
+                    email: "test@example.com"
+                }
+
+                return obj;
+            }
+        })
+    ],
+    pages: {
+        signIn: '/auth/login'
+    }
+}
