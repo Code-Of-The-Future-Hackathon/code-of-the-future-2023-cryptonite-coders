0d1c910bfa1db1d10235ef281bf52d2bc92070d2
Ivo <ivo.kraev2@protonmail.com>
2023-12-09 17:47:25 +0200

    feat: Create registration logic

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,27 @@
+import bcrypt from "bcryptjs";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        const { name, email, password } = await request.json();
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await db.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        return NextResponse.json(
+            { message: "User registered." },
+            { status: 201 },
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { message: "An error occurred while registering the user." },
+            { status: 500 },
+        );
+    }
+}
