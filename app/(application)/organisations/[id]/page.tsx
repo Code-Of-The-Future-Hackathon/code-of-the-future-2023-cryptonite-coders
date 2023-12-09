6975d6707e8a0ee69538fedfd68e764d9e88825b
Hristiyan <hristiyandimitrov26@itpg-varna.bg>
2023-12-09 23:43:39 +0200

    feat: Create organisation/[id] page.tsx

---
+++ app/(application)/organisations/[id]/page.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
+import OrganisationPost from "@/components/organisation/organisation-post";
+import { Typography } from "@/components/ui/typography";
+
+interface OrganisationPageProps {
+    params: {
+        id: string;
+    };
+}
+
+export default async function OrganisationPage({
+    params: { id },
+}: OrganisationPageProps) {
+    const organisation = await db.user.findFirst({
+        where: {
+            id: id,
+            role: "ORGANISATION",
+        },
+        select: {
+            id: true,
+            name: true,
+        },
+    });
+
+    const organisationPosts = await db.organisationPost.findMany({
+        where: {
+            authorId: id,
+        },
+        select: {
+            id: true,
+            title: true,
+            images: {
+                select: {
+                    image: true,
+                },
+            },
+        },
+    });
+
+    if (!organisation || organisationPosts.length <= 0) {
+        return notFound();
+    }
+
+    let isLeftDirection = true;
+
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-2 gap-3 sm:p-10">
+            <Typography variant="h1">{organisation.name}</Typography>
+            {organisationPosts.map((organisationPost, index) => (
+                <>
+                    <OrganisationPost
+                        direction={isLeftDirection ? "left" : "right"}
+                        title={organisationPost.title}
+                        images={organisationPost.images.map(
+                            (image) => image.image.path,
+                        )}
+                    />
+                    {(isLeftDirection = !isLeftDirection)}
+                </>
+            ))}
+        </div>
+    );
+}
