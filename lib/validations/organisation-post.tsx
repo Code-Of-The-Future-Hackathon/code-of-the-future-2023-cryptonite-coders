74c6e1c740e0876fb8bf13cd846ce7bbf352cdd1
Hristiyan <hristiyandimitrov26@itpg-varna.bg>
2023-12-09 18:35:52 +0200

    feat: Create organisation-post.tsx zod schema

---
+++ lib/validations/organisation-post.tsx
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { imageCreateSchema } from "@/lib/validations/image";
+
+export const organisationPostCreateSchema = z.object({
+    title: z.string(),
+    images: z.array(z.string().uuid()),
+});
+
+export type OrganisationPostCreate = z.infer<
+    typeof organisationPostCreateSchema
+>;
+
+export const organisationPostSchema = z.object({
+    id: z.string().uuid(),
+    title: z.string(),
+    images: z.array(imageCreateSchema),
+});
+
+export type OrganisationPost = z.infer<typeof organisationPostSchema>;
+
+export const organisationPostPatchSchema = z.object({
+    title: z.string(),
+});
+
+export type OrganisationPostPatch = z.infer<typeof organisationPostPatchSchema>;
