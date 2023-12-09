69b4bb15ba420d4505fe7544c6a7ee4e14a75c5f
Stoil100 <toli100ilov@gmail.com>
2023-12-09 09:13:38 +0200

    feat: Create carousel component

---
+++ components/carousel.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Carousel = () => {
+    return (
+        <div className="flex justify-center items-center border w-full">
+            <img src="https://picsum.photos/200/300" />
+        </div>
+    );
+};
