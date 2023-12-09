b7b83d17efa61db7158b85d5f7dd9c41ea20e064
Stoil100 <toli100ilov@gmail.com>
2023-12-09 12:31:27 +0200

    chore: Fix carousel logic and functionality

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+"use client"
 
-export const Carousel = () => {
-    return (
-        <div className="flex justify-center items-center border w-full">
-            <img src="https://picsum.photos/200/300" />
+import {
+  Carousel,
+  CarouselViewport,
+  CarouselItem,
+  CarouselContent,
+  CarouselControls,
+  CarouselTrigger,
+  CarouselPrev,
+  CarouselNext,
+} from "@/components/ui/carousel";
+import useEmblaCarousel from 'embla-carousel-react';
+
+const TEST_IMGS=["https://picsum.photos/200/300","https://picsum.photos/200/300","https://picsum.photos/200/300","https://picsum.photos/200/300"]
+
+export function CarouselBlock() {
+
+  return (
+    <Carousel className="max-w-xl space-y-4 py-4">
+      <CarouselViewport >
+        {TEST_IMGS.map((img) => (
+          <CarouselItem key={img} className="h-fit">
+            <CarouselContent>
+              <img src={img} className="w-auto max-h-[500px]"/>
+            </CarouselContent>
+          </CarouselItem>
+        ))}
+      </CarouselViewport>
+      <CarouselControls>
+        <div className="flex justify-between flex-row w-full">
+          <CarouselPrev />
+          <div className="space-x-2">
+          {TEST_IMGS.map((_, index) => (
+            <CarouselTrigger key={index} index={index}>
+              <span className="sr-only">{`e ${index + 1}`}</span>
+            </CarouselTrigger>
+          ))}
         </div>
-    );
-};
+          <CarouselNext />
+        </div>
+        
+      </CarouselControls>
+    </Carousel>
+  );
+}
