53b3b7d36d935101b14ab422806e37dfeecbb0e4
Stoil100 <toli100ilov@gmail.com>
2023-12-09 15:46:13 +0200

    feat: Create course child component

---
+++ components/Courses/course-child.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const CourseChild = () => {
+    return (
+        <div className="flex justify-center items-center flex-col min-h-screen px-24">
+            <video controls preload="metadata" className="w-full">
+                <source
+                    src="http://www.example.com/waterfall-video.mp4"
+                    type="video/mp4"
+                />
+                Video not supported.
+            </video>
+            <p className="text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                nostrum maiores sit rerum illum. Architecto cumque eos
+                accusantium voluptatum quo eius, id aperiam voluptatibus officia
+                commodi incidunt pariatur fugit necessitatibus?
+            </p>
+        </div>
+    );
+};
