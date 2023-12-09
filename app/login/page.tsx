e181f2740175ea3215aafc2632fe3b73e0616c81
Stoil100 <toli100ilov@gmail.com>
2023-12-09 10:49:28 +0200

    feat: Create login and signup page

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Login } from "@/components/forms/login";
+export default function LoginPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Login />
+        </main>
+    );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Register } from "@/components/forms/register";
+export default function SignupPage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Register />
+        </main>
+    );
+}
