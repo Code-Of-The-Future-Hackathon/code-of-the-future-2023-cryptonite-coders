998a5c0de27f8ffe19350062c790da52b78d0f06
Stoil100 <toli100ilov@gmail.com>
2023-12-09 15:46:41 +0200

    feat: Create pages for courses

---
+++ app/courses/[courseId]/page.tsx
@@ -0,0 +1,11 @@
+import { CourseChild } from '@/components/Courses/course-child'
+import React from 'react'
+
+export default function CourseChildPage() {
+  return (
+    <div className=' '>
+ <CourseChild/>
+    </div>
+   
+  )
+}
---
+++ app/courses/page.tsx
@@ -0,0 +1,15 @@
+import { CourseParent } from "@/components/Courses/course-parent";
+import React from "react";
+
+export default function Courses() {
+    return (
+        <main className="flex justify-center items-center p-4">
+            <div className="grid grid-cols-1 grid-rows-auto grid-flow-row gap-x-20 gap-y-5 lg:grid-cols-3 sm:grid-cols-2">
+            {Array.from({ length: 5 }, (_, index) => (
+                <CourseParent />
+            ))}
+            </div>
+            
+        </main>
+    );
+}
