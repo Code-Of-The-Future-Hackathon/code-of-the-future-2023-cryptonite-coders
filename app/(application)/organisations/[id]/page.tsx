e52d4711b235b5bf5110d3726ee43f4ab3fe583c
Hristiyan <hristiyandimitrov26@itpg-varna.bg>
2023-12-10 10:15:23 +0200

    feat: Display message when there is no posts

--- app/(application)/organisations/[id]/page.tsx
+++ app/(application)/organisations/[id]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { notFound } from "next/navigation";
 import OrganisationPost from "@/components/organisation/organisation-post";
 import { Typography } from "@/components/ui/typography";
+import { cn } from "@/lib/utils";
 
 interface OrganisationPageProps {
     params: {
@@ -39,27 +40,38 @@ export default async function OrganisationPage({
         },
     });
 
-    if (!organisation || organisationPosts.length <= 0) {
+    if (!organisation) {
         return notFound();
     }
 
     let isLeftDirection = true;
 
     return (
-        <div className="flex min-h-screen flex-col items-center justify-between p-2 gap-3 sm:p-10">
+        <div
+            className={cn(
+                "flex min-h-screen flex-col items-center p-2 gap-3 sm:p-10",
+                organisationPosts.length > 0 && "justify-between",
+            )}
+        >
             <Typography variant="h1">{organisation.name}</Typography>
-            {organisationPosts.map((organisationPost, index) => (
-                <>
-                    <OrganisationPost
-                        direction={isLeftDirection ? "left" : "right"}
-                        title={organisationPost.title}
-                        images={organisationPost.images.map(
-                            (image) => image.image.path,
-                        )}
-                    />
-                    {(isLeftDirection = !isLeftDirection)}
-                </>
-            ))}
+            {organisationPosts.length > 0 ? (
+                organisationPosts.map((organisationPost, index) => (
+                    <>
+                        <OrganisationPost
+                            direction={isLeftDirection ? "left" : "right"}
+                            title={organisationPost.title}
+                            images={organisationPost.images.map(
+                                (image) => image.image.path,
+                            )}
+                        />
+                        {(isLeftDirection = !isLeftDirection)}
+                    </>
+                ))
+            ) : (
+                <Typography variant="h4" className="mt-10">
+                    There is no posts!
+                </Typography>
+            )}
         </div>
     );
 }
