403aac279a9525eb7f01aba7de687203d353457e
Hristiyan <hristiyandimitrov26@itpg-varna.bg>
2023-12-10 10:41:44 +0200

    feat: Implement course fetching from database

--- app/(application)/courses/page.tsx
+++ app/(application)/courses/page.tsx
@@ -1,15 +1,44 @@
-import { CourseParent } from "@/components/courses/course-parent";
-
 import React from "react";
+import { db } from "@/lib/db";
+import { Typography } from "@/components/ui/typography";
+import CourseParent from "@/components/courses/course-parent";
 
-export default function Courses() {
-    return (
-        <main className="flex justify-center items-center p-4">
+export default async function Courses() {
+    const courses = await db.course.findMany({
+        select: {
+            id: true,
+            title: true,
+            author: true,
+            image: true,
+            description: true,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
+    return courses.length > 0 ? (
+        <div className="min-h-[calc(100vh-11rem)]">
             <div className="grid grid-cols-1 grid-rows-auto grid-flow-row gap-x-20 gap-y-5 lg:grid-cols-3 sm:grid-cols-2">
-                {Array.from({ length: 5 }, (_, index) => (
-                    <CourseParent />
-                ))}
+                <div className="flex justify-center items-center p-4">
+                    <div className="grid grid-cols-1 grid-rows-auto grid-flow-row gap-x-20 gap-y-5 lg:grid-cols-3 sm:grid-cols-2">
+                        {courses.map((course, index) => (
+                            <CourseParent
+                                key={index}
+                                id={course.id}
+                                title={course.title}
+                                author={course.author.name}
+                                image={course.image.path || ""}
+                                description={course.description}
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
-        </main>
+        </div>
+    ) : (
+        <Typography variant="h1" className="h-[calc(100vh-15rem)]">
+            There is no courses!
+        </Typography>
     );
 }
--- components/courses/course-parent.tsx
+++ components/courses/course-parent.tsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import { Button } from '../ui/button'
+import React from "react";
+import { Button } from "../ui/button";
+import { buttonVariants } from "../ui/button";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
 
-export const CourseParent = () => {
-  return (
-    <div className='flex justify-between flex-col p-3 border rounded-xl w-[300px] gap-3'>
-        <div className='flex items-center p-2 border-b-2 '>
-            <div className='flex items-center justify-center flex-col gap-1'>
-                <img className='rounded-full h-12 w-12' src='https://picsum.photos/200/300'/>
-                <p>Name</p>
+interface CourseParentProps {
+    id: string;
+    title: string;
+    author: string;
+    image: string;
+    description: string;
+}
+
+export default function CourseParent({
+    id,
+    title,
+    author,
+    image,
+    description,
+}: CourseParentProps) {
+    return (
+        <div className="flex justify-between flex-col p-3 border rounded-xl w-[300px] gap-3">
+            <div className="flex items-center p-2 border-b-2 ">
+                <div className="flex items-center justify-center flex-col gap-1">
+                    <img className="rounded-full h-12 w-12" src={image} />
+                    <p>{author}</p>
+                </div>
+                <p className="text-3xl flex-1 text-center">{title}</p>
             </div>
-            <p className='text-5xl flex-1 text-center'>Title</p>
+            <div>
+                <p>{description}</p>
+            </div>
+            <Button variant="default" disabled={true}>
+                Click for more...
+            </Button>
         </div>
-        <div>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, non iste quos voluptatum fuga dolorem similique doloremque, harum animi in debitis dolores ipsa suscipit, corrupti illum eos aliquid quisquam adipisci?</p>
-        </div>
-        <Button variant="default">Click for more...</Button>
-    </div>
-  )
+    );
 }
